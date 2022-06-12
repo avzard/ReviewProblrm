92162e95e9ff7263b1600dd828ec5d98cfd44c9a
Abhishek Jadhav <59304331+avzard@users.noreply.github.com>
2022-06-12 12:55:58 +0530

    Review Problem Date 10/06/22

---
+++ PrimeNumbersInRange.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practice_problems_1
+{
+    internal class PrimeNumbersInRange
+    {
+        public void PrimeNo()
+        {
+            int a, b, i, j, flag;
+            Console.WriteLine("Enter lower bound of " +"the interval: ");
+            a = int.Parse(Console.ReadLine());
+            Console.WriteLine("\nEnter upper bound " +"of the interval: ");            
+            b = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("\nPrime numbers between " +"{0} and {1} are: ", a, b);
+
+           
+            for (i = a; i <= b; i++)
+            {
+
+
+                if (i == 1 || i == 0)
+                {
+                    continue;
+                }
+
+                
+                flag = 1;
+
+                for (j = 2; j <= i / 2; ++j)
+                {
+                    if (i % j == 0)
+                    {
+                        flag = 0;
+                        break;
+                    }
+                }
+
+               
+                if (flag == 1)
+                    Console.WriteLine(i);
+            }
+        }
+    }
+
+}
+    
+
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using practice_problems_1;
+
+namespace Practice
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            // Pattern ptn = new Pattern(); 
+            //ptn.PATTERN();
+            //Float Flo = new Float();
+            //Flo.FLOATPROD();
+            PrimeNumbersInRange PRM= new PrimeNumbersInRange();
+            PRM.PrimeNo();
+        }
+    }
+}
